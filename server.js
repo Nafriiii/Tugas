9310f18cd7f55a1102f25fb0eaa2e81db03d7719
Nafriiii <email.irfanthoriq012@gmail.com>
2026-04-05 22:11:36 +0700

    cekcek

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'form.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/table', (req, res) => {
